b3d96ca84bd87a155caecb26aed41f9877300398
galvaitzman <38373111+galvaitzman@users.noreply.github.com>
2018-11-17 16:41:52 +0200

    init

---
+++ src/IDs.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class IDs 
+{
+	public static List<String> getIDs()
+	{
+		List<String> ids = new ArrayList<String>();
+		
+		String id1 = "308326032";
+		String id2 = "204328405";
+		//String id3 = "123456789";
+		
+		ids.add(id1);
+		ids.add(id2);
+		//ids.add(id3);
+		
+		return ids;
+	}
+}
